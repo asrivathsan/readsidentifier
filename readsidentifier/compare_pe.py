5706056c453e9ee7a12b2ecb2fb160ca484aec23
asrivathsan <asrivathsan@gmail.com>
2015-10-16 18:44:08 +0800

    Update compare_pe.py

--- readsidentifier/compare_pe.py
+++ readsidentifier/compare_pe.py
@@ -13,6 +13,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+#minor bug fix
 import sys,fileinput
 def consistpe(inputfile1,inputfile2,PathToDB,outfile):
 	DictEnd1={}
